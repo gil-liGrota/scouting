deeb74d7b5989a35bf66e6ddef2c4d0e082fb66e
gil-liGrota <gilgrota@gmail.com>
2025-06-21 05:31:14 +0300

    "working on the java"

--- app/src/main/java/com/example/scouting/loginCode.java
+++ app/src/main/java/com/example/scouting/loginCode.java
@@ -1,6 +1,8 @@
 package com.example.scouting;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,11 +12,15 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class loginCode extends AppCompatActivity {
 
+    private Button tester, scouter;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.login);
+        tester = findViewById(R.id.tester);
+        scouter = findViewById(R.id.scouter);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
